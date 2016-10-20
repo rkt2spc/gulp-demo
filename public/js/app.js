d2c0fef74dd6716b8c9196d99182706c60d1b389
Tuan Nguyen <nmtuan.dev@gmail.com>
2016-10-20 08:47:16 +0700

    Added potato component

--- public/js/app.js
+++ public/js/app.js
@@ -14,3 +14,21 @@ app.component('messagePrinting', {
 	templateUrl: '/partials/message.html',
 	controller: 'messagePrintingCtrl'
 })
+
+app.controller('potatoCtrl', ['$scope',
+	function($scope) {
+		$scope.toggleState = false;
+		$scope.toggleMessage = "Show potato";
+
+		$scope.toggleImage = function() {
+
+			$scope.toggleState = !$scope.toggleState;
+			$scope.toggleMessage = ($scope.toggleState)? "Hide potato" : "Show potato";
+		}
+	}
+])
+
+app.component('potato', {
+	templateUrl: '/partials/potato.html',
+	controller: 'potatoCtrl'
+})
